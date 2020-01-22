48dfb1ef15d9c172a27b545ad0d4c5df3688e446
Kat <katrina@madetech.com>
2020-01-22 14:34:43 +0000

    Add SQL files

---
+++ lib/backends/sql/fetchCustomer.sql
@@ -0,0 +1,6 @@
+SELECT 
+    customer_links.*, systems.name as system_name 
+FROM 
+    customer_links
+    JOIN systems ON systems.id = customer_links.system_id 
+    WHERE customer_id = @customer_id
