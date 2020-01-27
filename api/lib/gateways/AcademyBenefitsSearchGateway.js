59928eae2c7828c09e2056c21a68a3507358496b
Kat <katrina@madetech.com>
2020-01-27 16:52:00 +0000

    WIP buildSearchRecord tests

--- api/lib/gateways/AcademyBenefitsSearchGateway.js
+++ api/lib/gateways/AcademyBenefitsSearchGateway.js
@@ -1,28 +1,68 @@
 const path = require('path');
 const { loadSQL } = require('../Utils');
-const { searchCustomersBaseSQL } = loadSQL(path.join(__dirname, '../sql/AcademyBenefits'));
+const { Systems } = require('../Constants');
+const { searchCustomersBaseSQL } = loadSQL(
+  path.join(__dirname, '../sql/AcademyBenefits')
+);
 
 module.exports = options => {
-    const db = options.db
+  const db = options.db;
+  const buildSearchRecord = options.buildSearchRecord;
 
-    return {
-        execute: queryParams => {
-            let whereClause = [];
-            let params = [];
-
-            if(queryParams.firstName && queryParams.firstName !== '') {
-                params.push({
-                    id: 'forename',
-                    type: 'NVarChar',
-                    value: `%${queryParams.firstName.toUpperCase()}%`
-                });
-
-                whereClause.push('forename LIKE @forename');
-            }
-
-            const query = `${searchCustomersBaseSQL} AND(${whereClause.join(' AND ')})`;
-
-            db.request(query, params)
-        }
+  const search = async queryParams => {
+    let whereClause = [];
+    let params = [];
+    if (queryParams.firstName && queryParams.firstName !== '') {
+      params.push({
+        id: 'forename',
+        type: 'NVarChar',
+        value: `%${queryParams.firstName.toUpperCase()}%`
+      });
+      whereClause.push('forename LIKE @forename');
     }
-}
+    if (queryParams.lastName && queryParams.lastName !== '') {
+      params.push({
+        id: 'surname',
+        type: 'NVarChar',
+        value: `%${queryParams.lastName.toUpperCase()}%`
+      });
+      whereClause.push('surname LIKE @surname');
+    }
+    const query = `${searchCustomersBaseSQL} AND(${whereClause.join(' AND ')})`;
+    return await db.request(query, params);
+  };
+
+  const validateIds = record => {
+    return record.claim_id && record.check_digit && record.person_ref;
+  };
+
+  const processRecords = records => {
+    return records
+      .filter(record => validateIds(record))
+      .map(record => {
+        return buildSearchRecord({
+          id: `${record.claim_id}${record.check_digit}/${record.person_ref}`,
+          firstName: record.forename,
+          lastName: record.surname,
+          dob: record.birth_date,
+          nino: record.nino,
+          addr1: record.addr1,
+          addr2: record.addr2,
+          addr3: record.addr3,
+          addr4: record.addr4,
+          postcode: record.post_code,
+          source: Systems.ACADEMY_BENEFITS,
+          links: {
+            hbClaimId: record.claim_id
+          }
+        });
+      });
+  };
+
+  return {
+    execute: async queryParams => {
+      const records = await search(queryParams);
+      return processRecords(records);
+    }
+  };
+};
--- api/test/gateways/AcademyBenefitsSearchGateway.test.js
+++ api/test/gateways/AcademyBenefitsSearchGateway.test.js
@@ -1,31 +1,83 @@
 describe('Academy Benefits Search Gateway', () => {
-    let db;
+  let db;
 
-    beforeEach(() => {
-        db = {
-            request: jest.fn()
-        }
+  // const buildSearchRecord = jest.spyOn(
+  //   require('../../lib/models/BuildSearchRecord')
+  // );
 
-        academyBenefitsSearchGateway = require('../../lib/gateways/AcademyBenefitsSearchGateway')({ db });
+  const createGateway = records => {
+    db = {
+      request: jest.fn(async () => {
+        return Promise.resolve(records);
+      })
+    };
 
+    return require('../../lib/gateways/AcademyBenefitsSearchGateway')({
+      // buildSearchRecord,
+      db
     });
+  };
 
-    it('if the query contains firstname then the db is queried for forename', () => {
-        const firstName = 'maria'
-        const queryMatcher = expect.stringMatching(/forename LIKE @forename/);
-        const paramMatcher = expect.arrayContaining([expect.objectContaining({ value: `%${firstName.toUpperCase()}%` })]);
-        academyBenefitsSearchGateway.execute({ firstName });
-        expect(db.request).toHaveBeenCalledWith(queryMatcher, paramMatcher);
-    })
+  it('if the query contains firstname then the db is queried for forename', async () => {
+    const gateway = createGateway([]);
+    const firstName = 'maria';
+    const queryMatcher = expect.stringMatching(/forename LIKE @forename/);
+    const paramMatcher = expect.arrayContaining([
+      expect.objectContaining({ value: `%${firstName.toUpperCase()}%` })
+    ]);
+    await gateway.execute({ firstName });
+    expect(db.request).toHaveBeenCalledWith(queryMatcher, paramMatcher);
+  });
 
-    it('if the query does not have a firstname then the db is not queried for the forename', () => {
-        const queryMatcher = expect.not.stringMatching(/forename LIKE @forename/);
-        academyBenefitsSearchGateway.execute({ });
-        expect(db.request).toHaveBeenCalledWith(queryMatcher, expect.anything());
-    })
+  it('if the query does not have a firstname then the db is not queried for the forename', async () => {
+    const gateway = createGateway([]);
+    const queryMatcher = expect.not.stringMatching(/forename LIKE @forename/);
+    await gateway.execute({});
+    expect(db.request).toHaveBeenCalledWith(queryMatcher, expect.anything());
+  });
 
-    // it('if the query contains lastname then the db is queried for lastname', () => {
-    //     academyBenefitsSearchGateway.execute({ lastName: 'smith' });
-    //     expect(db.request).toHaveBeenCalledWith(expect.stringMatching(/lastname/), expect.objectContaining({ value: 'smith' }));
-    // })
-})
+  it('if the query contains lastname then the db is queried for lastname', async () => {
+    const gateway = createGateway([]);
+    const lastName = 'smith';
+    queryMatcher = expect.stringMatching(/surname LIKE/);
+    const paramMatcher = expect.arrayContaining([
+      expect.objectContaining({ value: `%${lastName.toUpperCase()}%` })
+    ]);
+    await gateway.execute({ lastName });
+    expect(db.request).toHaveBeenCalledWith(queryMatcher, paramMatcher);
+  });
+
+  it('if the query does not have a firstname then the db is not queried for the forename', async () => {
+    const gateway = createGateway([]);
+    const queryMatcher = expect.not.stringMatching(/surname LIKE @surname/);
+    await gateway.execute({});
+    expect(db.request).toHaveBeenCalledWith(queryMatcher, expect.anything());
+  });
+
+  // it('returns record if all IDs are valid', async () => {
+  //   const record = { claim_id: '123', check_digit: 'd', person_ref: '1' };
+  //   const gateway = createGateway([record]);
+
+  //   const records = await gateway.execute({});
+  //   expect(buildSearchRecord).toHaveBeenCalledTimes(1);
+  //   expect(records[0].id).toBe('123d/1');
+  // });
+
+  // it("doesn't return a record if any of the IDs are missing", async () => {
+  //   const gateway = createGateway([
+  //     { check_digit: 'd', person_ref: '1' },
+  //     { claim_id: '111', check_digit: 'x', person_ref: '2' }
+  //   ]);
+
+  //   const records = await gateway.execute({});
+  //   expect(buildSearchRecord).toHaveBeenCalledTimes(1);
+  //   expect(records.length).toBe(1);
+  //   expect(records[0].id).toBe('111x/2');
+  // });
+
+  // it('formats record data', async () => {
+  //   const gateway = createGateway([]);
+  //   const records = await gateway.execute();
+  //   expect(buildSearchRecord).toHaveBeenCalled();
+  // });
+});
