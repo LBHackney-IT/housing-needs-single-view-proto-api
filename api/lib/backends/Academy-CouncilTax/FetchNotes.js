908e3cc060e75b44f70881d84dc72fa4a6a1dd01
Matt Millar <matthew.millar@madetech.com>
2020-01-28 12:02:41 +0000

    Break academy council tax backend into parts

---
+++ api/lib/backends/Academy-CouncilTax/FetchNotes.js
@@ -0,0 +1,11 @@
+const Comino = require('../Comino');
+
+module.exports = () => {
+  return async account_ref => {
+    try {
+      return await Comino.fetchCustomerNotes({ account_ref });
+    } catch (err) {
+      console.log(`Error fetching customer notes in Comino: ${err}`);
+    }
+  };
+};
