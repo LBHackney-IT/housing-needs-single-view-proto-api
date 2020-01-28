15e6b782711d2f827fa77d74a7543e2acfcd15b6
Maria Astakhova <maria@madetech.com>
2020-01-28 11:05:43 +0000

    add search gateway to UHT Contacts

---
+++ api/lib/gateways/UHT-Contacts/UHTContactsSearchGateway.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const { loadSQL } = require('../../Utils');
+const { Systems } = require('../../Constants');
+const { searchCustomersBaseSQL } = loadSQL(path.join(__dirname, 'sql'));
+
+module.exports = options => {
+    const db = options.db;
+    const buildSearchRecord = options.buildSearchRecord;
+
+    let whereClause = [];
+    let params = [];
+
+    const search = async queryParams => {
+        if (queryParams.firstName && queryParams.firstName !== '') {
+            params.push({
+                id: 'forename',
+                type: 'NVarChar',
+                value: `%${queryParams.firstName.toUpperCase()}%`
+            });
+            whereClause.push(
+                'forename collate SQL_Latin1_General_CP1_CI_AS LIKE @forename'
+            )
+        }
+
+        if (queryParams.lastName && queryParams.lastName !== '') {
+            params.push({
+                id: 'surname',
+                type: 'NVarChar',
+                value: `%${queryParams.lastName.toUpperCase()}%`
+            });
+            whereClause.push(
+                'surname collate SQL_Latin1_General_CP1_CI_AS LIKE @surname'
+            )
+        };
+
+        whereClause = whereClause.map(clause => `(${clause})`);
+        const query = `${searchCustomersBaseSQL} WHERE (${whereClause.join(' AND ')})`;
+        return await db.request(query, params);
+    };
+
+    const processRecords = records => {
+        return records.map(record => {
+            buildSearchRecord({ id: `${record.house_ref.trim()}/${record.person_no}` })
+        })
+    };
+
+    return {
+        execute: async queryParams => {
+            const records = await search(queryParams);
+            return processRecords(records);
+        }
+    };
+}
---
+++ api/lib/gateways/UHT-Contacts/sql/fetchActionDiaryNotes.sql
@@ -0,0 +1,6 @@
+SELECT
+	araction.*
+FROM
+	araction
+	JOIN tenagree ON tenagree.tag_ref = araction.tag_ref
+WHERE tenagree.house_ref = @house_ref
