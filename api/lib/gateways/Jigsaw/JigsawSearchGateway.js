2815e1a85e1e67e9b73a3d6a4b3da69a251dfce7
Kat <katrina@madetech.com>
2020-01-29 16:54:53 +0000

    Add Jigsaw Search Gateway

---
+++ api/lib/gateways/Jigsaw/JigsawSearchGateway.js
@@ -0,0 +1,51 @@
+const { dedupe } = require('../../Utils');
+const { Systems } = require('../../Constants');
+
+module.exports = options => {
+  const buildSearchRecord = options.buildSearchRecord;
+  const doJigsawGetRequest = options.doJigsawGetRequest;
+  const jigsawEnv = options.jigsawEnv;
+
+  const searchUrl = `https://zebracustomers${jigsawEnv}.azurewebsites.net/api/customerSearch`;
+
+  const search = async query => {
+    const search = [query.firstName, query.lastName].filter(x => x).join(' ');
+    return await doJigsawGetRequest(searchUrl, { search });
+  };
+
+  const validateIds = record => {
+    return record.id;
+  };
+
+  const processRecords = records => {
+    if (records.length == 1 && records[0].id == 0) {
+      return [];
+    } else {
+      return dedupe(records, x => x.id)
+        .filter(record => validateIds(record))
+        .map(record => {
+          return buildSearchRecord({
+            id: record.id.toString(),
+            firstName: record.firstName,
+            lastName: record.lastName,
+            dob: record.doB,
+            nino: record.niNumber,
+            address: record.address,
+            source: Systems.JIGSAW
+          });
+        });
+    }
+  };
+
+  return {
+    execute: async queryParams => {
+      try {
+        const results = await search(queryParams);
+        return processRecords(results);
+      } catch (err) {
+        console.log(`Error searching customers in Jigsaw: ${err}`);
+        return [];
+      }
+    }
+  };
+};
--- api/test/gateways/Jigsaw/JigsawSearchGateway.test.js
+++ api/test/gateways/Jigsaw/JigsawSearchGateway.test.js
@@ -1,37 +1,103 @@
+const jigsawSearchGateway = require('../../../lib/gateways/Jigsaw/JigsawSearchGateway');
+
 describe('JigsawSearchGateway', () => {
   let buildSearchRecord;
-  let db;
+  let doJigsawGetRequest;
+  let jigsawEnv = 'test';
 
   const createGateway = (records, throwsError) => {
     buildSearchRecord = jest.fn(({ id }) => {
       return { id };
     });
 
-    db = {
-      request: jest.fn(async () => {
-        if (throwsError) {
-          return new Error('Database error');
-        }
-        return records;
-      })
-    };
+    doJigsawGetRequest = jest.fn(async (searchURL, search) => {
+      if (throwsError) {
+        return new Error('Database error');
+      }
+      return records;
+    });
 
-    return academyCouncilTaxSearchGateway({
+    return jigsawSearchGateway({
       buildSearchRecord,
-      db
+      doJigsawGetRequest,
+      jigsawEnv
     });
   };
 
-  it('if the query contains firstname then the db is queried for firstname', async () => {
+  it('calls doJigsawGetRequest gets with firstname and correct `url', async () => {
     const gateway = createGateway([]);
     const firstName = 'maria';
-    const queryMatcher = expect.stringMatching(/LIKE @full_name/);
-    const paramMatcher = expect.arrayContaining([
-      expect.objectContaining({ value: firstName.toUpperCase() })
-    ]);
-
+    const queryMatcher = expect.stringMatching(/maria/);
+    const searchUrl = expect.stringMatching(
+      /zebracustomerstest.azurewebsites.net/
+    );
     await gateway.execute({ firstName });
 
-    expect(db.request).toHaveBeenCalledWith(queryMatcher, paramMatcher);
+    expect(doJigsawGetRequest).toHaveBeenCalledWith(searchUrl, {
+      search: queryMatcher
+    });
+  });
+
+  it('if the query contains lastname then the db is queried for lastname', async () => {
+    const gateway = createGateway([]);
+    const lastName = 'smith';
+    const queryMatcher = expect.stringMatching(/smith/);
+
+    await gateway.execute({ lastName });
+
+    expect(doJigsawGetRequest).toHaveBeenCalledWith(expect.anything(), {
+      search: queryMatcher
+    });
+  });
+  it('if the query contains firstname andlastname then the db is queried for firstname and lastname', async () => {
+    const gateway = createGateway([]);
+    const firstName = 'maria';
+    const lastName = 'smith';
+    const queryMatcher = expect.stringMatching(/maria smith/);
+
+    await gateway.execute({ firstName, lastName });
+
+    expect(doJigsawGetRequest).toHaveBeenCalledWith(expect.anything(), {
+      search: queryMatcher
+    });
+  });
+
+  it('returns record if id exists', async () => {
+    const record = { id: 123 };
+    const gateway = createGateway([record]);
+
+    const records = await gateway.execute({});
+
+    expect(buildSearchRecord).toHaveBeenCalledTimes(1);
+    expect(records.length).toBe(1);
+    expect(records[0].id).toBe('123');
+  });
+
+  it('Does not return record if id is missing', async () => {
+    const record = {};
+    const gateway = createGateway([record]);
+
+    const records = await gateway.execute({});
+
+    expect(buildSearchRecord).toHaveBeenCalledTimes(0);
+    expect(records.length).toBe(0);
+  });
+
+  it('returns an empty set of records if error is thrown', async () => {
+    const record = { id: 123 };
+    const gateway = createGateway([record], true);
+
+    const records = await gateway.execute({});
+
+    expect(records.length).toBe(0);
+  });
+
+  it('filters out invalid records', async () => {
+    const record = { id: 0 };
+    const gateway = createGateway([record], true);
+
+    const records = await gateway.execute({});
+
+    expect(records.length).toBe(0);
   });
 });
