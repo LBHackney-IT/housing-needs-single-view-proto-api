b46d72009cd2c30e3781efccc96495b03f3cc33a
Maria Astakhova <maria@madetech.com>
2020-01-30 15:39:01 +0000

    moved all gateways into dependencies, now only use case is called in index

--- api/lib/libDependencies.js
+++ api/lib/libDependencies.js
@@ -1,56 +1,6 @@
 const { doJigsawGetRequest, jigsawEnv } = require('./JigsawUtils');
 const SqlServerConnection = require('./SqlServerConnection');
-const jigsawSearchGateway = require('./gateways/Jigsaw/JigsawSearchGateway')({
-  doJigsawGetRequest,
-  jigsawEnv,
-  buildSearchRecord: require('./entities/SearchRecord')()
-});
-const academyBenefitsSearchGateway = require('./gateways/Academy-Benefits/AcademyBenefitsSearchGateway')(
-  {
-    db: new SqlServerConnection({
-      dbUrl: process.env.ACADEMY_DB
-    }),
-    buildSearchRecord: require('./entities/SearchRecord')()
-  }
-);
-const UHTContactsSearchGateway = require('./gateways/UHT-Contacts/UHTContactsSearchGateway')(
-  {
-    db: new SqlServerConnection({
-      dbUrl: process.env.UHT_DB
-    }),
-    buildSearchRecord: require('./entities/SearchRecord')()
-  }
-);
-const UHTHousingRegisterSearchGateway = require('./gateways/UHT-HousingRegister/UHTHousingRegisterSearchGateway')(
-  {
-    db: new SqlServerConnection({
-      dbUrl: process.env.UHT_DB
-    }),
-    buildSearchRecord: require('./entities/SearchRecord')()
-  }
-);
-const AcademyCouncilTaxSearchGateway = require('./gateways/Academy-CouncilTax/AcademyCouncilTaxSearchGateway')(
-  {
-    db: new SqlServerConnection({
-      dbUrl: process.env.ACADEMY_DB
-    }),
-    buildSearchRecord: require('./entities/SearchRecord')()
-  }
-);
-const UHWSearchGateway = require('./gateways/UHW/UHWSearchGateway')({
-  db: new SqlServerConnection({
-    dbUrl: process.env.UHW_DB
-  }),
-  buildSearchRecord: require('./entities/SearchRecord')()
-});
-const singleViewSearchGateway = require('./gateways/SingleView/SingleViewSearchGateway')(
-  {
-    db: require('./PostgresDb'),
-    buildSearchRecord: require('./entities/SearchRecord')()
-  }
-);
-
-const customerSearch = require('./use-cases/CustomerSearch');
+const buildSearchRecord = require('./entities/SearchRecord')();
 
 const cleanRecord = require('./use-cases/CleanRecord')({
   badData: {
@@ -59,14 +9,71 @@ const cleanRecord = require('./use-cases/CleanRecord')({
   }
 });
 
+const jigsawSearchGateway = require('./gateways/Jigsaw/JigsawSearch')({
+  doJigsawGetRequest,
+  jigsawEnv,
+  buildSearchRecord
+});
+
+const academyBenefitsSearchGateway = require('./gateways/Academy-Benefits/AcademyBenefitsSearch')(
+  {
+    db: new SqlServerConnection({
+      dbUrl: process.env.ACADEMY_DB
+    }),
+    buildSearchRecord
+  }
+);
+const UHTContactsSearchGateway = require('./gateways/UHT-Contacts/UHTContactsSearch')(
+  {
+    db: new SqlServerConnection({
+      dbUrl: process.env.UHT_DB
+    }),
+    buildSearchRecord
+  }
+);
+const UHTHousingRegisterSearchGateway = require('./gateways/UHT-HousingRegister/UHTHousingRegisterSearch')(
+  {
+    db: new SqlServerConnection({
+      dbUrl: process.env.UHT_DB
+    }),
+    buildSearchRecord
+  }
+);
+const AcademyCouncilTaxSearchGateway = require('./gateways/Academy-CouncilTax/AcademyCouncilTaxSearch')(
+  {
+    db: new SqlServerConnection({
+      dbUrl: process.env.ACADEMY_DB
+    }),
+    buildSearchRecord
+  }
+);
+const UHWSearchGateway = require('./gateways/UHW/UHWSearch')({
+  db: new SqlServerConnection({
+    dbUrl: process.env.UHW_DB
+  }),
+  buildSearchRecord
+});
+const singleViewSearchGateway = require('./gateways/SingleView/SingleViewSearch')(
+  {
+    db: require('./PostgresDb'),
+    buildSearchRecord
+  }
+);
+
+const customerSearch = require('./use-cases/CustomerSearch')({
+  cleanRecord,
+  gateways: [
+    jigsawSearchGateway,
+    academyBenefitsSearchGateway,
+    UHTContactsSearchGateway,
+    UHTHousingRegisterSearchGateway,
+    AcademyCouncilTaxSearchGateway,
+    UHWSearchGateway,
+    singleViewSearchGateway
+  ],
+  groupSearchRecords: require('./use-cases/GroupSearchRecords')()
+});
+
 module.exports = {
-  jigsawSearchGateway,
-  academyBenefitsSearchGateway,
-  UHTContactsSearchGateway,
-  UHTHousingRegisterSearchGateway,
-  AcademyCouncilTaxSearchGateway,
-  UHWSearchGateway,
-  singleViewSearchGateway,
-  customerSearch,
-  cleanRecord
+  customerSearch
 };
