636f85ad3730b4b29c2062c8841f021708eabe2b
Maria Astakhova <maria@madetech.com>
2020-02-13 15:33:43 +0000

    add more tests to the date comparison function

--- api/test/Utils.test.js
+++ api/test/Utils.test.js
@@ -24,8 +24,24 @@ describe('Utils', () => {
     expect(result).toStrictEqual(expected);
   });
 
-  it('orders one records by start date in descending order', async () => {
-    const result = records.sort(compareDateStrings);
-    expect(result).toStrictEqual(expected);
+  it('does not change order when a date is absent', async () => {
+    const record = [{ start_date: '2019-03-20' }, { start_date: '' }];
+    const result = record.sort(compareDateStrings);
+
+    expect(result).toStrictEqual(record);
+  });
+
+  it('does not change order when a date is absent (example two)', async () => {
+    const record = [{ start_date: '' }, { start_date: '2019-03-20' }];
+    const result = record.sort(compareDateStrings);
+
+    expect(result).toStrictEqual(record);
+  });
+
+  it('It returns nothing when given an empty array', async () => {
+    const record = [];
+    const result = record.sort(compareDateStrings);
+
+    expect(result).toStrictEqual(record);
   });
 });
