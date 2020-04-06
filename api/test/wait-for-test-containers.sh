02de000082350aa648b64b7a4ef0dce44b1b572f
Kat <katrina@madetech.com>
2020-04-06 09:25:53 +0100

    Cleanup

--- api/test/wait-for-test-containers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-until [ "`$(which docker) inspect -f {{.State.Running}} housing-needs-single-view-proto-api_test_db_1`" == "true" ] && [ "`$(which docker) inspect -f {{.State.Running}} housing-needs-single-view-proto-api_uh_test_db_1`" == "true" ] || ([ "`$(which docker) inspect -f {{.State.Running}} repo_test_db_1`" == "true" ] && [ "`$(which docker) inspect -f {{.State.Running}} repo_uh_test_db_1`" == "true" ]) ; do
-    echo 'Test container not yet running, waiting...';
-    sleep 1;
-done;
-echo 'Test container running, starting tests';
-exit 0;
