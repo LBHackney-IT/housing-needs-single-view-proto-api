160a1f11f69d367a24b603117443ce5d260991be
Elena <elena@madetech.com>
2020-04-29 14:44:01 +0100

    update route handling to report to sentry

--- api/index.js
+++ api/index.js
@@ -12,19 +12,16 @@ const {
   deleteCustomer
 } = require('./lib/libDependencies');
 
+let Sentry;
 if (process.env.ENV === 'staging' || process.env.ENV === 'production') {
-  const Sentry = require('@sentry/node');
+  Sentry = require('@sentry/node');
 
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
     environment: process.env.ENV
   });
 
-  // The request handler must be the first middleware on the app
   app.use(Sentry.Handlers.requestHandler());
-
-  // The error handler must be before any other error middleware and after all controllers
-  app.use(Sentry.Handlers.errorHandler());
 }
 
 app.use(bodyParser.json());
@@ -43,54 +40,81 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get('/customers', async (req, res) => {
-  const q = querystring.stringify(req.query);
-  console.log(`CUSTOMER SEARCH "${q}"`);
-  console.time(`CUSTOMER SEARCH "${q}"`);
-  const results = await customerSearch(req.query);
-  console.timeEnd(`CUSTOMER SEARCH "${q}"`);
-  res.send(results);
+app.get('/customers', async (req, res, next) => {
+  try {
+    const q = querystring.stringify(req.query);
+    console.log(`CUSTOMER SEARCH "${q}"`);
+    console.time(`CUSTOMER SEARCH "${q}"`);
+    const results = await customerSearch(req.query);
+    console.timeEnd(`CUSTOMER SEARCH "${q}"`);
+    res.send(results);
+  } catch (err) {
+    next(err);
+  }
 });
 
-app.post('/customers', async (req, res) => {
-  console.log('SAVING CUSTOMER');
-  console.time('SAVING CUSTOMER');
-  // Save the selected customer records
-  const customer = await saveCustomer(req.body.customers);
-  console.timeEnd('SAVING CUSTOMER');
-  res.send({ customer });
+app.post('/customers', async (req, res, next) => {
+  try {
+    console.log('SAVING CUSTOMER');
+    console.time('SAVING CUSTOMER');
+    // Save the selected customer records
+    const customer = await saveCustomer(req.body.customers);
+    console.timeEnd('SAVING CUSTOMER');
+    res.send({ customer });
+  } catch (err) {
+    next(err);
+  }
 });
 
-app.delete('/customers/:id', async (req, res) => {
-  console.log(`DELETE CUSTOMER id="${req.params.id}"`);
-  console.time(`DELETE CUSTOMER id="${req.params.id}"`);
-  await deleteCustomer(req.params.id);
-  console.timeEnd(`DELETE CUSTOMER id="${req.params.id}"`);
-  res.sendStatus(200);
+app.delete('/customers/:id', async (req, res, next) => {
+  try {
+    console.log(`DELETE CUSTOMER id="${req.params.id}"`);
+    console.time(`DELETE CUSTOMER id="${req.params.id}"`);
+    await deleteCustomer(req.params.id);
+    console.timeEnd(`DELETE CUSTOMER id="${req.params.id}"`);
+    res.sendStatus(200);
+  } catch (err) {
+    next(err);
+  }
 });
 
-app.get('/customers/:id/record', async (req, res) => {
-  console.log(`GET CUSTOMER id="${req.params.id}"`);
-  console.time(`GET CUSTOMER id="${req.params.id}"`);
-  const result = await fetchRecords(req.params.id);
-  console.timeEnd(`GET CUSTOMER id="${req.params.id}"`);
-  res.send({ customer: result });
+app.get('/customers/:id/record', async (req, res, next) => {
+  try {
+    console.log(`GET CUSTOMER id="${req.params.id}"`);
+    console.time(`GET CUSTOMER id="${req.params.id}"`);
+    const result = await fetchRecords(req.params.id);
+    console.timeEnd(`GET CUSTOMER id="${req.params.id}"`);
+    res.send({ customer: result });
+  } catch (err) {
+    next(err);
+  }
 });
 
-app.get('/customers/:id/notes', async (req, res) => {
-  console.log(`GET CUSTOMER NOTES id="${req.params.id}"`);
-  console.time(`GET CUSTOMER NOTES id="${req.params.id}"`);
-  const results = await fetchNotes(req.params.id, res.locals.hackneyToken);
-  console.timeEnd(`GET CUSTOMER NOTES id="${req.params.id}"`);
-  res.send(results);
+app.get('/customers/:id/notes', async (req, res, next) => {
+  try {
+    console.log(`GET CUSTOMER NOTES id="${req.params.id}"`);
+    console.time(`GET CUSTOMER NOTES id="${req.params.id}"`);
+    const results = await fetchNotes(req.params.id, res.locals.hackneyToken);
+    console.timeEnd(`GET CUSTOMER NOTES id="${req.params.id}"`);
+    res.send(results);
+  } catch (err) {
+    next(err);
+  }
 });
 
-app.get('/customers/:id/documents', async (req, res) => {
-  console.log(`GET CUSTOMER DOCS id="${req.params.id}"`);
-  console.time(`GET CUSTOMER DOCS id="${req.params.id}"`);
-  const results = await fetchDocuments(req.params.id, res.locals.hackneyToken);
-  console.timeEnd(`GET CUSTOMER DOCS id="${req.params.id}"`);
-  res.send(results);
+app.get('/customers/:id/documents', async (req, res, next) => {
+  try {
+    console.log(`GET CUSTOMER DOCS id="${req.params.id}"`);
+    console.time(`GET CUSTOMER DOCS id="${req.params.id}"`);
+    const results = await fetchDocuments(
+      req.params.id,
+      res.locals.hackneyToken
+    );
+    console.timeEnd(`GET CUSTOMER DOCS id="${req.params.id}"`);
+    res.send(results);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = {
