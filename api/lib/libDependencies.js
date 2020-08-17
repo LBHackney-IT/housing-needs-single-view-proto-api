db3cd0772f95ad2b8f6d2201a45ac14907bc2ab1
elcorbs <emma@madetech.com>
2020-08-17 11:24:52 +0100

    Use API to search for customers in academy and remove code that was contacting the db

--- api/lib/libDependencies.js
+++ api/lib/libDependencies.js
@@ -65,23 +65,11 @@ const jigsawSearchGateway = require('./gateways/Jigsaw/Search')({
   buildSearchRecord,
   logger
 });
-const academyBenefitsSearchDb = require('./gateways/Academy-Benefits/SearchDb')(
-  {
-    db: academyDb,
-    buildSearchRecord
-  }
-);
-const academyBenefitsSearchAPI = require('./gateways/Academy-Benefits/SearchAPI')(
-  {
-    baseUrl: process.env.ACADEMY_API_BASE_URL,
-    apiKey: process.env.ACADEMY_API_API_KEY,
-    buildSearchRecord
-  }
-);
 const academyBenefitsSearchGateway = require('./gateways/Academy-Benefits/Search')(
   {
-    searchDb: academyBenefitsSearchDb,
-    searchAPI: academyBenefitsSearchAPI,
+    buildSearchRecord,
+    baseUrl: process.env.ACADEMY_API_BASE_URL,
+    apiKey: process.env.ACADEMY_API_API_KEY,
     logger
   }
 );
