6364e3a9f7dda47c66c4b6bf60898602d1c5c012
Tony Griffin <tony@madetech.com>
2020-08-19 14:42:39 +0100

    sv-19/Remove hard coded response object in UHT-tenancies gateway

--- api/lib/gateways/UHT-Tenancies/FetchTenancy.js
+++ api/lib/gateways/UHT-Tenancies/FetchTenancy.js
@@ -8,39 +8,39 @@ module.exports = options => {
   const logger = options.logger;
 
   async function fetchTenancy(tag_ref) {
-    // return await db.request(fetchTenancySQL, [
-    //   { id: 'tag_ref', type: 'NVarChar', value: tag_ref }
-    // ]);
-    return {
-      id: '123456/1',
-      address: '1 Hill Street N16 5TT',
-      type: 'Secure',
-      startDate: '1992-06-13',
-      residents: [
-        {
-          title: 'Mrs',
-          forename: 'Joan',
-          surname: 'Fisher',
-          fullName: 'Mrs Joan Fisher',
-          dob: '1970-02-30',
-          mobileNum: '07777123456',
-          homeNum: '02088881234',
-          workNum: '02012345678',
-          email: 'mjf@email.com'
-        },
-        {
-          title: 'Miss',
-          forename: 'Sally',
-          surname: 'Fisher',
-          fullName: 'Miss Sally Fisher',
-          dob: '1990-03-21',
-          mobileNum: '07777456789',
-          homeNum: '02088881234',
-          workNum: '02077775678',
-          email: 'sallyfisher90@email.com'
-        }
-      ]
-    };
+    return await db.request(fetchTenancySQL, [
+      { id: 'tag_ref', type: 'NVarChar', value: tag_ref }
+    ]);
+    // return {
+    //   id: '123456/1',
+    //   address: '1 Hill Street N16 5TT',
+    //   type: 'Secure',
+    //   startDate: '1992-06-13',
+    //   residents: [
+    //     {
+    //       title: 'Mrs',
+    //       forename: 'Joan',
+    //       surname: 'Fisher',
+    //       fullName: 'Mrs Joan Fisher',
+    //       dob: '1970-02-30',
+    //       mobileNum: '07777123456',
+    //       homeNum: '02088881234',
+    //       workNum: '02012345678',
+    //       email: 'mjf@email.com'
+    //     },
+    //     {
+    //       title: 'Miss',
+    //       forename: 'Sally',
+    //       surname: 'Fisher',
+    //       fullName: 'Miss Sally Fisher',
+    //       dob: '1990-03-21',
+    //       mobileNum: '07777456789',
+    //       homeNum: '02088881234',
+    //       workNum: '02077775678',
+    //       email: 'sallyfisher90@email.com'
+    //     }
+    //   ]
+    // };
   }
 
   const processTenancy = function(tenancy) {
