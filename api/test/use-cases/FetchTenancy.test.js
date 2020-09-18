89afa6f4ef6a5e6aa28ac044e69b12a4eb633bbd
Kat <katrina@madetech.com>
2020-09-18 11:08:45 +0100

    Add fetch tasks gateway test and refactir fetchTenancy use case test

--- api/test/use-cases/FetchTenancy.test.js
+++ api/test/use-cases/FetchTenancy.test.js
@@ -1,77 +1,74 @@
 describe('FetchTenancy', () => {
-  let fetchTenancyGateway;
-  let matServiceFetchContactsGateway;
-  let dummyContactsGatewayResponse;
-  let dummyTenancyGatewayResponse;
+  const dummyTenancyGatewayResponse = {
+    id: '123456/1',
+    address: '12 Hill Street N16 5TT',
+    type: 'Secure',
+    startDate: '1992-06-13',
+    uprn: '123456'
+  };
+  const dummyContactsGatewayResponse = [
+    {
+      title: 'Mrs',
+      firstName: 'Joan',
+      lastName: 'Fisher',
+      dateOfBirth: '1970-02-30',
+      mobileNum: '07777123456',
+      homeNum: '02088881234',
+      workNum: '02012345678',
+      emailAddress: 'mjf@email.com'
+    }
+  ];
+  const dummyTenantsGatewayResponse = [
+    {
+      personNo: 1,
+      responsible: true,
+      relationship: 'relationship',
+      title: 'Mrs',
+      firstName: 'Joan',
+      lastName: 'Fisher',
+      dateOfBirth: '1970-02-30',
+      telephone1: '07777123456',
+      telephone2: '02088881234',
+      telephone3: '02012345678',
+      emailAddress: 'mjf@email.com'
+    }
+  ];
+  const fetchTenancyGateway = {
+    execute: jest.fn(() => dummyTenancyGatewayResponse)
+  };
+  const fetchTenantsGateway = {
+    execute: jest.fn(() => dummyTenantsGatewayResponse)
+  };
 
-  beforeEach(() => {
-    dummyTenancyGatewayResponse = {
-      id: '123456/1',
-      address: '12 Hill Street N16 5TT',
-      type: 'Secure',
-      startDate: '1992-06-13',
-      uprn: '123456'
-    };
-    dummyContactsGatewayResponse = [
-      {
-        title: 'Mrs',
-        firstName: 'Joan',
-        lastName: 'Fisher',
-        dateOfBirth: '1970-02-30',
-        mobileNum: '07777123456',
-        homeNum: '02088881234',
-        workNum: '02012345678',
-        emailAddress: 'mjf@email.com'
-      }
-    ];
-    dummyTenantsGatewayResponse = [
-      {
-        personNo: 1,
-        responsible: true,
-        relationship: 'relationship',
-        title: 'Mrs',
-        firstName: 'Joan',
-        lastName: 'Fisher',
-        dateOfBirth: '1970-02-30',
-        telephone1: '07777123456',
-        telephone2: '02088881234',
-        telephone3: '02012345678',
-        emailAddress: 'mjf@email.com'
-      }
-    ];
-    fetchTenancyGateway = {
-      execute: jest.fn(() => dummyTenancyGatewayResponse)
-    };
-    fetchTenantsGateway = {
-      execute: jest.fn(() => dummyTenantsGatewayResponse)
-    };
+  const matServiceFetchContactsGateway = {
+    execute: jest.fn(() => dummyContactsGatewayResponse)
+  };
+  const matServiceFetchTasksGateway = {
+    execute: jest.fn(() => dummyContactsGatewayResponse)
+  };
 
-    matServiceFetchContactsGateway = {
-      execute: jest.fn(() => dummyContactsGatewayResponse)
-    };
-
-    fetchTenancy = require('../../lib/use-cases/FetchTenancy')({
-      fetchTenancyGateway,
-      matServiceFetchContactsGateway,
-      fetchTenantsGateway
-    });
-
-    mergedResponse = [
-      {
-        personNo: 1,
-        relationship: 'relationship',
-        responsible: true,
-        title: 'Mrs',
-        firstName: 'Joan',
-        lastName: 'Fisher',
-        dateOfBirth: '1970-02-30',
-        telephone1: '07777123456',
-        telephone2: '02088881234',
-        telephone3: '02012345678',
-        emailAddress: 'mjf@email.com'
-      }
-    ];
+  const fetchTenancy = require('../../lib/use-cases/FetchTenancy')({
+    fetchTenancyGateway,
+    matServiceFetchContactsGateway,
+    fetchTenantsGateway,
+    matServiceFetchTasksGateway
   });
+
+  const mergedResponse = [
+    {
+      personNo: 1,
+      relationship: 'relationship',
+      responsible: true,
+      title: 'Mrs',
+      firstName: 'Joan',
+      lastName: 'Fisher',
+      dateOfBirth: '1970-02-30',
+      telephone1: '07777123456',
+      telephone2: '02088881234',
+      telephone3: '02012345678',
+      emailAddress: 'mjf@email.com'
+    }
+  ];
 
   it('can fetch a tenancy from the Tenancy Gateway and return it', async () => {
     const id = 1;
